ab490ff0cd7143b1436435b0207d006acec88f64
ip.osinga <ip.osinga@alfa-college.nl>
2024-01-17 11:03:09 +0100

    2024-01-17

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+let activeCellId = 1;
+
 let sudoku_string = "<ul>";
 for(i=1; i<=81; i++){
-    sudoku_string += "<li><input class='form-control text-center' style='font-size: 52px;' value='" + i + "'></li>";
+    sudoku_string += "<li id='" + i + "' onclick=\"activateCell(" + i + ")\"><span></span></li>";
 }
 sudoku_string += "</ul>";
 document.getElementById("sudoku").innerHTML = sudoku_string;
+
+function selectCijfer(cijfer){
+    document.getElementById(activeCellId).innerText = cijfer;
+}
+
+function activateCell(id){
+    activeCellId = id;
+}
